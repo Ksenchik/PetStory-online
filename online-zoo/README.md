7bc1d23b911ea3fff5c3dc58c10a92a7042a315b
Ksenchik <oksana.100@mail.ru>
2022-10-28 17:18:23 +0300

    fix: add slick slider

---
+++ online-zoo/README.md
@@ -0,0 +1,2 @@
+#Ksenchik
+https://ksenchik.github.io/PetStory-online/online-zoo/pages/main/
--- online-zoo/pages/main/script.js
+++ online-zoo/pages/main/script.js
@@ -4,95 +4,104 @@ const menuModal = document.querySelector(".header__modal");
 const contentBody = document.querySelector("body");
 
 menuButton.onclick = function () {
-   menuButton.classList.toggle("active");
-   menuButton1.classList.toggle("active");
-   menuModal.classList.toggle("active");
-   contentBody.classList.toggle("lock");
-}
+  menuButton.classList.toggle("active");
+  menuButton1.classList.toggle("active");
+  menuModal.classList.toggle("active");
+  contentBody.classList.toggle("lock");
+};
 menuButton1.onclick = function () {
-   menuButton.classList.remove("active");
-   menuButton1.classList.remove("active");
-   menuModal.classList.remove("active");
-   contentBody.classList.remove("lock");
-}
+  menuButton.classList.remove("active");
+  menuButton1.classList.remove("active");
+  menuModal.classList.remove("active");
+  contentBody.classList.remove("lock");
+};
 menuModal.onclick = function () {
-   menuButton.classList.remove("active");
-   menuButton1.classList.remove("active");
-   menuModal.classList.remove("active");
-   contentBody.classList.remove("lock");
-}
+  menuButton.classList.remove("active");
+  menuButton1.classList.remove("active");
+  menuModal.classList.remove("active");
+  contentBody.classList.remove("lock");
+};
 
 // PETS CAROUSEL
-const swiper = new Swiper('.pets__slider', {
-   navigation: {
-      nextEl: '.btn-next',
-      prevEl: '.btn-prev'
-   },
-   spaceBetween: 30,
-   slidesPerView: 3,
-   slidesPerColumn: 2,
-   autoHeight: false,
-   grid: {
-      rows: 2,      
-    },
-   loop: true,
-   loopedSlides:4,
-   breakpoints: {
-      320: {
-         slidesPerView: 1,
+$(document).ready(function () {
+  $(".slider").slick({
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 700,
+    easing: "ease",
+    rows: 2,
+    responsive: [
+      {
+        breakpoint: 950,
+        settings: {
+          slidesToShow: 2,
+        },
       },
-      550: {
-         slidesPerView: 2,
+      {
+        breakpoint: 620,
+        settings: {
+          slidesToShow: 1,
+        },
       },
-      980: {
-         slidesPerView: 3,
-      }
-   },
+    ],
+  });
+
+  $(".btn-prev").click(function (event) {
+    $(".slider").slick("slickPrev");
+  });
+  $(".btn-next").click(function (event) {
+    $(".slider").slick("slickNext");
+  });
 });
 
 // POPUP
 document.querySelector(".testimonials__flex").addEventListener("click", (e) => {
-   Popup.show(e)
+  Popup.show(e);
 });
 
 document.querySelector(".popup").addEventListener("click", (e) => {
-   Popup.close(e)
+  Popup.close(e);
 });
 
 const Popup = {
-   show(e) {
-     const card = e.target.closest(".testimonials-card")
-     console.log(card)
-     if (card) {
-       const content = document.querySelector(".popup__content")
-       document.querySelector(".popup").classList.add("popup_show")
-       content.innerHTML = ""
-       content.append(card.cloneNode(true))
-     }
-   },
- 
-   close(e) {
-     if (!e.target.classList.contains("popup__container")) {
-       document.querySelector(".popup").classList.remove("popup_show")
-     }
-   }
+  show(e) {
+    const card = e.target.closest(".testimonials-card");
+    console.log(card);
+    if (card) {
+      const content = document.querySelector(".popup__content");
+      document.querySelector(".popup").classList.add("popup_show");
+      content.innerHTML = "";
+      content.append(card.cloneNode(true));
+    }
+  },
+
+  close(e) {
+    if (!e.target.classList.contains("popup__container")) {
+      document.querySelector(".popup").classList.remove("popup_show");
+    }
+  },
 };
 
 // TESTIMONIALS
 document.querySelector(".testimonials-range").addEventListener("input", () => {
-   Scroller.goto(document.querySelector(".testimonials-range").value)
+  Scroller.goto(document.querySelector(".testimonials-range").value);
 });
 
 const Scroller = {
-   calculateRange() {
-     const range = document.querySelector(".testimonials-card")
-     range.max = window.innerWidth <= 1000 ? 9 : 8
-   },
- 
-   goto(gapOrder) {
-     const offset = (document.querySelector(".testimonials-card").getBoundingClientRect().width + 30) * gapOrder
-     document.querySelector(".testimonials-card").style.marginLeft = `-${offset}px`
-   }
- }
+  calculateRange() {
+    const range = document.querySelector(".testimonials-card");
+    range.max = window.innerWidth <= 1000 ? 9 : 8;
+  },
 
-
+  goto(gapOrder) {
+    const offset =
+      (document.querySelector(".testimonials-card").getBoundingClientRect()
+        .width +
+        30) *
+      gapOrder;
+    document.querySelector(
+      ".testimonials-card"
+    ).style.marginLeft = `-${offset}px`;
+  },
+};
